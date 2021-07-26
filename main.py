94598e9816a63ed800cbf3737c18769ad73bef3a
PCi1 <87982813+PCi1@users.noreply.github.com>
2021-07-26 15:16:22 +0200

    updated product list

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-product = ["roquefort", "stilton", "brie", "gouda", "edam", "parmezan", "mozzarella", "czechosłowacki ser zo owczego mleka"]
-price = [12.50, 11.24, 9.30 , 8.55 , 11, 16.50, 14, 122.32]
+product = ["roquefort", "stilton", "brie", "gouda", "edam", "parmezan", "mozzarella", "czechosłowacki ser zo owczego mleka", "chleb"]
+price = [12.50, 11.24, 9.30 , 8.55 , 11, 16.50, 14, 122.32, 3]
 for i in range(len(product)):
     a='{:0.2f}'.format(price[i])
     b='{:36.36}'.format(product[i])
